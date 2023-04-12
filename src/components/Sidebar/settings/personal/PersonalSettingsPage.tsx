875e026e9a65efeffc23b7684b1dad0f2551da09
privetyaolega <privetyaolega@gmail.com>
2023-04-12 12:47:20 +0700

    upd: auth test + settings page

--- src/components/Sidebar/settings/personal/PersonalSettingsPage.tsx
+++ src/components/Sidebar/settings/personal/PersonalSettingsPage.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import {Divider, Row} from "antd";
+import React, {useState} from "react";
+import {Divider, Form, InputNumber, Row, Switch} from "antd";
 
 export const PersonalSettingsPage: React.FC = () => {
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const [countdownTime, setCountdownTime] = useState<number | undefined>(undefined);
+
+    const handleSwitchChange = (checked: boolean) => {
+        setIsEnabled(checked);
+    };
+
+    const handleCountdownTimeChange = (value: number | null) => {
+        if (value) {
+            setCountdownTime(value);
+        }
+    };
 
     return (
         <>
             <Divider plain>Personal settings</Divider>
-                <Row>
+            <Row>
+                <Form layout="horizontal" style={{paddingLeft: 100}}>
 
-                </Row>
+                    <Form.Item label="Precount enabled" valuePropName="checked">
+                        <Switch
+                            checked={isEnabled}
+                            onChange={handleSwitchChange}
+                            checkedChildren="ON"
+                            unCheckedChildren="OFF"
+                        />
+                    </Form.Item>
+                    <Form.Item label="Precount value">
+                        <InputNumber
+                            disabled={!isEnabled}
+                            min={1}
+                            max={60}
+                            defaultValue={countdownTime}
+                            onChange={handleCountdownTimeChange}
+                        />
+                    </Form.Item>
+                </Form>
+
+            </Row>
             <Divider/>
         </>
     );
--- src/page/TestComponent.tsx
+++ src/page/TestComponent.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
+import jwt_decode from 'jwt-decode';
+import CredentialResponse = google.accounts.id.CredentialResponse;
 
+const clientId = "665344838178-0p0t79qgdq6lubuv400nra06p8lae7m3.apps.googleusercontent.com"
+
+interface CredentialPayload {
+    name?: string;
+    picture?: string;
+}
 
 const TestComponent: React.FC = () => {
+
+    const [user, setUser] = React.useState<CredentialPayload | null>()
+
+    function handleCallbackResponse(response: CredentialResponse) {
+        console.log("Encoded JWT ID token: " + response.credential)
+        const userObject: CredentialPayload = jwt_decode<CredentialPayload>(response.credential)
+        setUser(userObject);
+        console.log(userObject);
+        // document.getElementById("signInDiv")!.hidden = true;
+
+    }
+
+    function handleSignOut(e: any) {
+        setUser(null);
+    }
+
+    React.useEffect(() => {
+        google.accounts.id.initialize({
+            client_id: clientId,
+            callback: handleCallbackResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv")!,
+            {
+                type: "standard", // or "icon"
+                theme: "outline",
+                size: "large"
+            }
+        );
+    }, []);
 
 
     return (
         <div>
-
+            <div id="signInDiv">
+                {user &&
+                    <div>
+                        <button onClick={(e) => handleSignOut(e)}>SIGN OUT</button>
+                        <div>
+                            <img src={user?.picture}></img>
+                            <h3>{user?.name} </h3>
+                        </div>
+                    </div>
+                }
+            </div>
 
         </div>
     );
